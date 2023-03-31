84af4a5c34a04a745face36ba6608cd1f867633a
RamaRamdani03 <ramaramdani449@gamil.com>
2023-03-31 15:45:12 +0700

    void swap

--- Quicksort_098/Quicksort_098.cpp
+++ Quicksort_098/Quicksort_098.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
-int cmp_count = 0; // Number of comprasion 
-int mov_count = 0; // Number of data movements 
+
+int arr[20];            // Array of integers to hold values
+int cmp_count = 0;      // Number of comparison
+int mov_count = 0;      // Number of data movements
 int n;
 
-void input() 
+void input() {
+    while (true)
+    {
+        cout << "Masukkan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+
+    cout << "\n-------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n-------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+}
+
+// Swaps the element st index x with the element at index y
+void swap(int x, int y)
